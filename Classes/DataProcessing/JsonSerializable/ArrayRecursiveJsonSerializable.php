c4bfbfe729da7eeb5be48660e9e7485ad0e1d7dc
Steffen Mächtel <s.maechtel@netzbewegung.com>
2024-12-10 20:34:31 +0100

    Add flexform json serializable

--- Classes/DataProcessing/JsonSerializable/ArrayRecursiveJsonSerializable.php
+++ Classes/DataProcessing/JsonSerializable/ArrayRecursiveJsonSerializable.php
@@ -6,6 +6,7 @@ namespace Netzbewegung\NbHeadlessContentBlocks\DataProcessing\JsonSerializable;
 
 use JsonSerializable;
 use TYPO3\CMS\Core\Collection\LazyRecordCollection;
+use TYPO3\CMS\Core\Domain\FlexFormFieldValues;
 use TYPO3\CMS\Core\LinkHandling\TypolinkParameter;
 use TYPO3\CMS\Core\Resource\Collection\LazyFileReferenceCollection;
 use TYPO3\CMS\Core\Resource\FileReference;
@@ -25,6 +26,9 @@ class ArrayRecursiveJsonSerializable implements JsonSerializable
             switch (true) {
                 case is_array($value):
                     $data[$key] = new ArrayRecursiveJsonSerializable($value);
+                    break;
+                case $value instanceof FlexFormFieldValues:
+                    $data[$key] = $value->toArray();
                     break;
                 case $value instanceof TypolinkParameter:
                     $data[$key] = new TypolinkParameterJsonSerializable($value);
