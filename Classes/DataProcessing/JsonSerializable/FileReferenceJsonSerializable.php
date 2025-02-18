070545016212e3b4f8057f42d58c023f8cf76c08
Carsten Wilhelm <carsten.wilhelm@macaw.net>
2025-02-18 16:58:48 +0100

    Added some properties for link / files

--- Classes/DataProcessing/JsonSerializable/FileReferenceJsonSerializable.php
+++ Classes/DataProcessing/JsonSerializable/FileReferenceJsonSerializable.php
@@ -6,6 +6,8 @@ namespace Netzbewegung\NbHeadlessContentBlocks\DataProcessing\JsonSerializable;
 
 use JsonSerializable;
 use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class FileReferenceJsonSerializable implements JsonSerializable
 {
@@ -16,10 +18,22 @@ class FileReferenceJsonSerializable implements JsonSerializable
 
     public function jsonSerialize(): mixed
     {
+        $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+        /** @var \TYPO3\CMS\Core\Site\Entity\Site $site */
+        $site = current($siteFinder->getAllSites());
+        $apiUrl = rtrim($site->getConfiguration()['base'], '/');
+        $frontendUrl = rtrim($site->getConfiguration()['frontendBase'], '/');
+
+        $url = $this->fileReference->getPublicUrl();
+        $scheme = parse_url($url, PHP_URL_SCHEME);
+        if ($scheme === null) {
+            $url = $apiUrl . $url;
+        }
+
         return [
             'alt' => $this->fileReference->getAlternative(),
             'title' => $this->fileReference->getTitle(),
-            'publicUrl' => $this->fileReference->getPublicUrl()
+            'publicUrl' => $url,
         ];
     }
 }
--- Classes/DataProcessing/JsonSerializable/TypolinkParameterJsonSerializable.php
+++ Classes/DataProcessing/JsonSerializable/TypolinkParameterJsonSerializable.php
@@ -32,12 +32,18 @@ class TypolinkParameterJsonSerializable implements JsonSerializable
                 'url' => $linkResult->getUrl(),
                 'target' => $linkResult->getTarget(),
                 'type' => $linkResult->getType(),
+                'title' => $linkResult->getLinkText(),
+                'config' => $linkResult->getLinkConfiguration(),
+                "attr" => $linkResult->getAttributes()
             ];
         } catch (UnableToLinkException $e) {
             return [
                 'url' => '',
                 'target' => '',
                 'type' => '',
+                'title' => '',
+                'config' => [],
+                "attr" => [],
                 '__errorMessage' => $e->getMessage()
             ];
         }
